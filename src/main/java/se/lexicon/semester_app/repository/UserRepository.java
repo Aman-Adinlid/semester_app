d991e7c1acdb7ddd36f058d0643600817d7363ba
aman adinlid <95pinkpanda@gmail.com>
2021-06-10 16:35:03 +0200

    ##Edited classes##

--- src/main/java/se/lexicon/semester_app/repository/UserRepository.java
+++ src/main/java/se/lexicon/semester_app/repository/UserRepository.java
@@ -3,8 +3,6 @@ package se.lexicon.semester_app.repository;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.semester_app.entity.User;
 
-import java.util.Optional;
-
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByEmail(String email);
--- src/main/java/se/lexicon/semester_app/repository/VacationDayRepository.java
+++ src/main/java/se/lexicon/semester_app/repository/VacationDayRepository.java
@@ -1,7 +1,6 @@
 package se.lexicon.semester_app.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import se.lexicon.semester_app.dto.VacationDayDto;
 import se.lexicon.semester_app.entity.VacationDay;
 
 import java.time.LocalDate;
