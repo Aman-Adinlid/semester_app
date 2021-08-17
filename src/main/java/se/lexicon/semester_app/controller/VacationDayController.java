0ec39f6aa6e8355cffca427a0632f87ae4366beb
Ahmad-Abdou <kyoub1996@gmail.com>
2021-08-17 12:22:32 +0200

    Adding request to the employee

--- src/main/java/se/lexicon/semester_app/controller/VacationDayController.java
+++ src/main/java/se/lexicon/semester_app/controller/VacationDayController.java
@@ -31,7 +31,7 @@ public class VacationDayController {
 
     @GetMapping()
     public ResponseEntity<List<VacationDayDto>> findAll() {
-
+        System.out.println();
         return ResponseEntity.status(HttpStatus.OK).body(vacationDayService.findAll());
     }
 
